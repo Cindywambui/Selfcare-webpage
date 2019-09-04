6e660d5bed5193f662d061518761a299d82bd6d8
christylydia <christinegikandi@gmail.com>
2019-09-04 11:57:40 +0300

    add js,html changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); 
-// }
 
     
-document.getElementById("ps").onsubmit(function(){
-    yes =parseInt(document.querySelector('input[name="yes"]:checked').value);
+// document.getElementById("ps").onsubmit(function(){
+//     yes =parseInt(document.querySelector('input[name="yes"]:checked').value);
     
     
     // document.getElementById("form1").onsubmit=function() {
     //     variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
+
+
+    $(document).ready(function () {
+
+        $("img#psil").click(function () {
+            $(".psol").toggle(1000);
+    
+        });
+        $("img#ssil").click(function () {
+            $(".ssol").toggle(1000);
+    
+        });
+    
+        $("img#msil").click(function () {
+            $(".msol").toggle(1000);
+    
+        });
+    });
