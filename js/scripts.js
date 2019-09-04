aaba6a45aa076f9924a16e3fb8ae85f341899449
SharonFaith <sharonfaith15@gmail.com>
2019-09-04 14:02:03 +0300

    use js to show and hide the text contents in the cards

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@ $(document).ready(function() {
   });
   $("button#hide-lilly").click(function() {
     $(".lilly-hidden").hide();
-  })
+  });
+  $("#patrick").click(function() {
+    $(".patrick-hidden").show();
+  });
+  $("button#hide-patrick").click(function() {
+    $(".patrick-hidden").hide();
+  });
+  $("#scarlet").click(function() {
+    $(".scarlet-hidden").show();
+  });
+  $("button#hide-scarlet").click(function() {
+    $(".scarlet-hidden").hide();
+  });
+  $("#drew").click(function() {
+    $(".drew-hidden").show();
+  });
+  $("button#hide-drew").click(function() {
+    $(".drew-hidden").hide();
+  });
 });
