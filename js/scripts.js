d16fc73e9c33d7a4d6e74f20b562ed078b3a376b
christylydia <christinegikandi@gmail.com>
2019-09-05 08:35:07 +0300

    change js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-$(document).ready(function(){
-
-$("button#physical").click(function(){
-$("div.physicals").fadeOut();
-$("div.socials").fadeIn();
-});
-$("button#social").click(function(){
-    $("div.social").fadeOut();
-    $("div.Mental").fadeIn();
-    });
-    $("button#mental").click(function(){
-        $("div.Mental").fadeOut();
-        $("div.emotional").fadeIn();
-        });
-        $("button#emotional").click(function(){
-            $("div.emotional").fadeOut();
-            $("div.buttons").fadeIn();
-            });
-            
-});
-
-
-
 
 
 class FitnessCheck {
